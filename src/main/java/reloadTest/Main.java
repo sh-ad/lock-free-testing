fd8d9ada063e27cfd59403616f26f5e3a6f9eb22
sh_ad <alex1097@mail.ru>
2020-02-21 20:39:27 +0300

    add pom

--- src/main/java/reloadTest/Main.java
+++ src/main/java/reloadTest/Main.java
@@ -6,8 +6,8 @@ import java.util.concurrent.atomic.AtomicReference;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        AtomicReference<Integer> atom = new AtomicReference();
-        atom.compareAndSet(32, 32);
-        System.out.println("");
+        AtomicReference<Integer> atom = new AtomicReference(32);
+        atom.compareAndSet(32, 64);
+        System.out.println(atom);
     }
 }
